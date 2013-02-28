e27532f7f2c58017ebf157b91a68ceb5c30f2c0b
Allen Chen <allen.sl.chen@gmail.com>
2013-02-27 21:09:12 -0800

    More TODOs outlining what I want to do

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Rules Enforcement Server
 ====
 
 An enforcement server for a very special set of rules.
+
+TODO
+====
+
+* >>Split Card() class into CardData() and PlayingCard()<<
+* What's the best way to represent the global gamestate?  We pass it around everywhere as a parameter to every function but functions get cluttered with gamestate parameters.
+* objects.py needs some love (ie organization into more than one file)
+* Client
+* Tests
--- objects.py
+++ objects.py
@@ -1,5 +1,8 @@
 import random
 
+# TODO(allenchen): [organization] These enums belong in a
+# separate file.
+
 class Enum:
     pass
 
@@ -36,12 +39,24 @@ PHASES.MAIN2 = 10
 PHASES.END = 11
 PHASES.CLEANUP = 12
 
+# TODO(allenchen): Subtype enum.  ie. Goblin, Merfolk, Elf, ...
+# [organization] obviously move that to another file. subtypes.py
+
+# TODO(allenchen): Counter type. (eg. +1/+1 counters, Poison counters,
+# -1/-1 counters, feather counters, etc.)
+
+# TODO(allenchen): Special static effects. (eg. static effects from
+# Olivia that makes things into vampires not expressed through counters)
+
 EFFECT_ID = 0
 CARD_ID = 0
 PERMANENT_ID = 0
 SPELL_ID = 0
 
-class ManaCost(object):
+class Cost(object):
+    pass
+
+class ManaCost(Cost):
     def __init__(self):
         self.white = 0
         self.blue = 0
@@ -50,8 +65,16 @@ class ManaCost(object):
         self.green = 0
         self.colorless = 0
 
+class SpecialCost(Cost):
+    def __init__(self):
+        # TODO(allenchen): Abstraction for "special" cost?
+        # This includes things like additional costs, optional
+        # effects, etc.
+        pass
+
 class ManaRepository(object):
     def __init__(self):
+        # all: integers
         self.white = 0
         self.blue = 0
         self.black = 0
@@ -60,6 +83,7 @@ class ManaRepository(object):
         self.colorless = 0
 
     def canPay(self, cost):
+        # cost: ManaCost()
         return (self.white >= cost.white and
             self.blue >= cost.blue and
             self.black >= cost.black and
@@ -68,6 +92,7 @@ class ManaRepository(object):
             self.colorless >= cost.colorless)
 
     def spendMana(self, cost):
+        # cost: ManaCost()
         if not self.canPay(cost):
             return False
         self.white -= cost.white
@@ -78,6 +103,8 @@ class ManaRepository(object):
         self.colorless -= cost.colorless
 
 class CardAttributes(object):
+    # TODO(allenchen): Make subclasses:
+    # PermanentAttributes(), SpellAttributes(), etc.
     def __init__(self,
                  colors=list(),
                  types=list(),
@@ -85,7 +112,15 @@ class CardAttributes(object):
                  subtypes=list(),
                  power=None,
                  toughness=None,
-                 owner=None):
+                 owner=None,
+                 special=list()):
+        # colors: list of [E]COLOR : integer
+        # types: list of [E]CARD_TYPES : integer
+        # costs: list of Cost() - both Mana and Special costs
+        # subtypes: list of [E]SUBTYPES : integer
+        # power: integer (?? - do I care about unhinged)
+        # toughness: integer (?? - do I care about unhinged)
+        # owner: Player()
         self.colors = colors
         self.types = types
         self.costs = costs
@@ -93,6 +128,10 @@ class CardAttributes(object):
         self.power = power
         self.toughness = toughness
         self.owner = owner
+        # Cards in graveyards and exile can have counters on them.
+        # fuck you whoever made that idea.
+        # fuck you whoever let that idea through.
+        self.special = list()
 
     def isPermanentCard(self):
         return any(
@@ -140,9 +179,11 @@ class GameState(object):
         self.priority = 0
 
     def addPermanent(self, permanent):
+        # permanent: Permanent()
         self.permanents[permanent.controller] += [permanent]
 
     def drawCard(self, player):
+        # player: Player()
         if len(self.libraries[player]) == 0:
             # TODO(allenchen): This isn't right since there should be a special event
             # for drawing from an empty library.  You don't necessarily lose the game
@@ -157,6 +198,8 @@ class GameState(object):
         return card
 
     def addCard(self, card, player):
+        # card: Card()
+        # player: Player()
         self.hands[player] += [card]
 
     def applyEffect(self, effect):
@@ -214,7 +257,12 @@ class Effect(object):
         EFFECT_ID += 1
 
     def addAction(self, action):
+        # Actions ARE order sensitive; they should be added with care.
         self.actions += [action]
+
+    def apply(self, gamestate):
+        for action in self.actions:
+            action(gamestate)
 
 class Permanent(object):
     def __init__(self, attributes):
@@ -234,27 +282,43 @@ class Permanent(object):
 
 class Spell(object):
     def __init__(self, attributes):
+        # attributes: CardAttributes()
+        # id: integer
+        # TODO(allenchen): attributes should be SpellAttributes() <= CardAttributes()
         self.attributes = attributes
         self.id = SPELL_ID
         SPELL_ID += 1
 
     def permanentize(self, player):
+        # player: Player()
+        # TODO(allenchen): Make Player() class.
         # Turns the spell into a permanent under the player's control.
         if not self.isPermanentCard:
             return None
         # TODO(allenchen): Create a permanent under PLAYER's control.
 
 class Card(object):
+    # TODO(allenchen): This needs to split.  There should be TWO kinds of cards:
+    # actual Card information (which are singletons) and Card as in cards in library
+    # that will be played. [urgent]
     def __init__(self, attributes):
+        # attributes: CardAttributes()
+        # id: integer
         self.attributes = attributes
         self.id = CARD_ID
         CARD_ID += 1
 
 class DelayedEffect(object):
-    def __init__(self):
-        self.triggerCondition = lambda state: False
-        self.effect = lambda state: state
+    def __init__(self, triggerCondition, effect):
+        # triggerCondition: lambda gamestate: [[returns true|false]]
+        # effect: lambda gamestate: [[void]]
+        self.triggerCondition = triggerCondition
+        self.effect = effect
 
-    def trigger(self):
+    def trigger(self, gamestate):
+        # gamestate: GameState()
         # TODO(allenchen): How do we represent the global gamestate?
         self.effect(gamestate)
+
+    def test(self, gamestate):
+        return self.triggerCondition(gamestate)
